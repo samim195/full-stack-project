de68cfb7ff2f28c104bb6cf31d20f7c4a6dd6c69
samimkohistani <samimkohistani@hotmail.com>
2020-12-23 20:26:30 +0000

    adding mysql db

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var mysql = require("mysql");
 const port = 3000;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 9000,
+    // socketPath: 'mysql-socket-path',
+    database: 'randomsapp'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    console.log("Successfully connected to the MySQL Server");
+})
+
+connection.end(function(err) {
+    if (err) {
+      return console.log('error:' + err.message);
+    }
+    console.log('Close the database connection.');
+  });
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
